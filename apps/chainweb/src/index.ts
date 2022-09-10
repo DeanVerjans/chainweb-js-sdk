3b2554a975f3e873e7015d198c3b15946406633c
Dean <developer.dean@outlook.com>
2022-09-10 21:42:23 +0200

    Remove http-provider from chainweb

--- apps/chainweb/src/index.ts
+++ apps/chainweb/src/index.ts
@@ -1,9 +1,5 @@
-import { ChainOptions, Provider, HttpProvider } from 'chainweb-sdk-core';
+import { ChainOptions } from 'chainweb-sdk-core';
 
 export class ChainWeb {
-  private readonly _provider: Provider;
-
-  constructor(options?: ChainOptions) {
-    this._provider = new HttpProvider(options);
-  }
+  constructor(options?: ChainOptions) {}
 }
