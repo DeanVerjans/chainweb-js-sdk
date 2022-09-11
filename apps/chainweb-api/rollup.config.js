3a4d39abad42fe86744645acabd2045816dfff12
Dean Verjans <40548730+DeanVerjans@users.noreply.github.com>
2022-09-11 14:42:20 +0200

    [GENERAL] Rollup building (#2)
    * Add rollup building for packages
    
    * Fix rollup building config issue

---
+++ apps/chainweb-api/rollup.config.js
@@ -0,0 +1,48 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+
+const config = [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        typescript: ttypescript,
+        useTsconfigDeclarationDir: true,
+        emitDeclarationOnly: true,
+      }),
+      json(),
+    ],
+    output: {
+      file: 'dist/es/index.js',
+      format: 'es',
+      sourcemap: true,
+      exports: 'auto',
+    },
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        typescript: ttypescript,
+        useTsconfigDeclarationDir: true,
+        emitDeclarationOnly: true,
+      }),
+      nodeResolve({
+        preferBuiltins: true,
+      }),
+      commonjs(),
+      json(),
+    ],
+    output: {
+      file: 'dist/cjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'auto',
+    },
+  },
+];
+export default config;
