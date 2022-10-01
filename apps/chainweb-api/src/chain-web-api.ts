ad3e50252388fb1b1332b0bde42a0407160e10d1
Dean Verjans <Developer.dean@outlook.com>
2022-10-01 23:16:23 +0200

    expose block hashes in the api

--- apps/chainweb-api/src/chain-web-api.ts
+++ apps/chainweb-api/src/chain-web-api.ts
@@ -1,19 +1,26 @@
 import { ChainOptions, HttpProvider } from 'chainweb-sdk-core';
 import { BlockHeaderEndpoint } from './endpoints';
+import { BlockHashEndpoint } from './endpoints/block-hash.endpoint';
 
 export class ChainWebApi {
   private readonly _httpProvider: HttpProvider;
 
   // Api Endpoints
   private readonly _blockHeader: BlockHeaderEndpoint;
+  private readonly _blockHash: BlockHashEndpoint;
 
   constructor(options: ChainOptions) {
     this._httpProvider = new HttpProvider(options);
 
     this._blockHeader = new BlockHeaderEndpoint(this._httpProvider);
+    this._blockHash = new BlockHashEndpoint(this._httpProvider);
   }
 
   public get blockHeader() {
     return this._blockHeader;
+  }
+
+  public get blockHash() {
+    return this._blockHash;
   }
 }
